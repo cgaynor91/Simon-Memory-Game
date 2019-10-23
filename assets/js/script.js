07bb1bb05a9b3cb8cf9fe0dc0661421b9e34d4c7
EC2 Default User <ec2-user@ip-172-31-90-208.ec2.internal>
2019-10-23 12:20:26 +0000

    Created function to return coloured pads to original state

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,101 @@ $(strictButton).on('click', function() {
     strict = false;
   }
 });
+
+
+// Intialize Game when start button clicked
+
+$(startButton).on("click", function() {
+  clearInterval(intervalId);
+  playGame();
+});
+
+// This function runs when it is time for the computer to generate a sequence. 
+
+
+function playGame() {
+  intervalId = 0;
+  win = false;
+  $(turnCounter).text("1");
+  playerOrder = [];
+  turn = 1;
+  flash = 0;
+  good = true;
+  for (var i = 0; i < 20; i++) {
+    order.push(Math.floor(Math.random() * 4) + 1);
+  }
+  compTurn = true;
+
+  intervalId = setInterval(gamePlay, 800);
+}
+
+/* A function to identify if it is the player's turn or the computer's turn. 
+The amount of flashes must match the number in the turn counter - sequence stops and is logged.
+The comptuer reproduces a sequence and checks light id and applies correct light and sound function */
+
+function gamePlay() {
+
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+  }
+
+  if (compTurn) {
+    clearColor();
+    setTimeout(function() {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      if (order[flash] == 4) four();
+      flash++;
+    }, 200);
+  }
+}
+
+// Functions that generate lights and sounds for all four pads
+
+function one() {
+  if (volume) {
+    let audio = document.getElementById("sound-green");
+    audio.play();
+  }
+  volume = true;
+  greenPad.style.backgroundColor = "#39ff14";
+}
+
+function two() {
+  if (volume) {
+    let audio = document.getElementById("sound-red");
+    audio.play();
+  }
+  volume = true;
+  redPad.style.backgroundColor = "#B22222";
+}
+
+function three() {
+  if (volume) {
+    let audio = document.getElementById("sound-yellow");
+    audio.play();
+  }
+  volume = true;
+  yellowPad.style.backgroundColor = "#FFFF00";
+}
+
+function four() {
+  if (volume) {
+    let audio = document.getElementById("sound-blue");
+    audio.play();
+  }
+  volume = true;
+  bluePad.style.backgroundColor = "#0000ff";
+}
+
+// Returns pads to their original state
+
+function clearColor() {
+  greenPad.style.backgroundColor = "#4CBB17";
+  redPad.style.backgroundColor = "#a30b0b";
+  yellowPad.style.backgroundColor = "#daa520";
+  bluePad.style.backgroundColor = "#00008b";
+}
